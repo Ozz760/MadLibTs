e0a84bc4d1265235dadee481c6cb9029c764234e
Oscar Silva <oscarsilva2356@gmail.com>
2023-04-09 14:17:43 -0700

    bg started:

--- src/components/IntroText.tsx
+++ src/components/IntroText.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const IntroText = () => {
-  return <div>IntroText</div>;
+  return (
+    <div className="w-full h-screen bg-gradient-to-b from-slate-500 to-white">
+      <h1 className="text-center text-3xl text-slate-300">Mad Libs</h1>
+      <h3> Start the game by giving a noun</h3>
+    </div>
+  );
 };
 
 export default IntroText;
